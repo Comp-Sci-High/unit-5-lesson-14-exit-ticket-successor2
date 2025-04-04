ac22a53ca070761df79a678b98d6c7d46e036649
successor2 <110046839+successor2@users.noreply.github.com>
2025-04-04 14:30:31 +0000

    done

--- index.js
+++ index.js
@@ -33,5 +33,12 @@ const Potion = mongoose.model("Potion", potionSchema, "Potions")
 // Create a dynamic route handler that updates a specific potion's color based on its label [2 pts]
 // e.g. /update/polyjuice will update the color of the polyjuice potion to pearly white
 // e.g. /update/amortentia will update the color of the Amortentia potion to golden purple
+app.patch("/update/:label", async (req, res) => {
+  const updatedColor = await Creature.findOneAndUpdate(
+    {label: req.params.label},
+    {color: req.body.color},
+  )
 
+  res.json(updatedColor)
+})
 
--- public/script.js
+++ public/script.js
@@ -1,4 +1,27 @@
 // Create and call a function that tests your update route handler [2 pts]
 // Make sure that the function takes the name of the potion as a parameter
 // There are already potion's with the label polyjuice or amortentia
+console.log("Script running...")
 
+const base = "https://scaling-carnival-gj79j44vpqpfwq5g-3000.app.github.dev"
+
+async function patchCreature(label) {
+    try {
+        // fill in the path
+        const response = await fetch(base + "/update/" + label, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                color: "black",
+            })
+
+        });
+        const json = await response.json();
+        console.log(json);
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+patchLabel("potion")
